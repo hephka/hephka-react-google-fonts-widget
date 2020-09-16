1ddcc2fce5f2b468c04e1658352c7ee9a9a7d6fa
hephka <franckthevenin95@gmail.com>
2020-09-16 16:13:01 +0200

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className="p-5 bg-danger text-white text-center">
+          <h1 className="display-1">Quoi de neuf, Google Fonts&trade; ?</h1>
+          <p className="h4">
+              "Les plus récentes, les plus "
+              <i lang="en">trendy</i>
+              " et les plus populaires polices Google."
+          </p>
+      </header>
+  )
+}
+
+export default Header
